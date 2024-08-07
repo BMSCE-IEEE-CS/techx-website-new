85f77b4f7d83d1899a41a8b0d7d34d93c37279bb
Aneesh <aneeshkp1515@gmail.com>
2024-08-07 11:33:11 +0530

    Countdown timer butified

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import Navbar from "@/components/navbar";
 import localFont from "next/font/local";
 import Link from "next/link";
-import useCountdown from "@/components/CountdownTimer";  
+import useCountdown from "@/components/CountdownTimer";
 
 const spacex = localFont({ src: "../app/SpaceX.ttf" });
 
@@ -15,12 +15,27 @@ export default function Home() {
       <div className="absolute inset-0 bg-gradient-to-tr from-mOrange via-black to-black opacity-70 z-0"></div>
       <div className="relative z-10 flex flex-col ">
         <Navbar />
-        <div className="relative flex flex-col items-center bg-no-repeat bg-cover">
+        <div className="relative flex flex-col items-center bg-no-repeat bg-cover select-none cursor-default">
           <div className="flex flex-col mt-52">
             <div className="text-center mb-10">
-              <h2 className="text-3xl text-white font-bold -mt-110">COMING SOON!</h2>
-              <div className="text-white text-2xl mt-4">
-                {days}d {hours}h {minutes}m {seconds}s
+              <h2 className="text-3xl text-white font-bold -mt-[130px]">COMING SOON!</h2> <br />
+              <div className="flex space-x-4 mt-4">
+                <div className="flex flex-col items-center bg-gray-800 rounded-lg p-4" style={{ fontSize: "90px", width:"100px" }}>
+                  <span className="text-4xl text-mOrange"  style={{fontWeight:"600"}}>{days}</span>
+                  <span className="text-lg text-white"  style={{fontWeight:"600"}}>Days</span>
+                </div>
+                <div className="flex flex-col items-center bg-gray-800 rounded-lg p-4" style={{ fontSize: "90px" , width:"100px" }}>
+                  <span className="text-4xl text-mOrange" style={{fontWeight:"600"}}>{hours}</span>
+                  <span className="text-lg text-white"  style={{fontWeight:"600"}}>Hours</span>
+                </div>
+                <div className="flex flex-col items-center bg-gray-800 rounded-lg p-4" style={{ fontSize: "90px" , width:"100px" }}>
+                  <span className="text-4xl text-mOrange"  style={{fontWeight:"600"}}>{minutes}</span>
+                  <span className="text-lg text-white" style={{fontWeight:"600"}}>Minutes</span>
+                </div>
+                <div className="flex flex-col items-center bg-gray-800 rounded-lg p-4" style={{ fontSize: "90px" , width:"100px" }}>
+                  <span className="text-4xl text-mOrange"  style={{fontWeight:"600"}}>{seconds}</span>
+                  <span className="text-lg text-white"  style={{fontWeight:"600"}}>Seconds</span>
+                </div>
               </div>
             </div>
             <div>
